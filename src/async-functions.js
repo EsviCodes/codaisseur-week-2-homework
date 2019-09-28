b618257fa1e1b6ef45b9b2f0b0918f597a798c58
EsviCodes <esther.js.vis@gmail.com>
2019-09-28 12:27:41 +0200

    Return promise from function getTatooineResidents

--- src/async-functions.js
+++ src/async-functions.js
@@ -3,12 +3,13 @@ const superagent = require("superagent");
 const getTatooineResidents = () => {
   superagent
     .get("https://swapi.co/api/planets/1/")
-    .then(res => {
-      console.log(res.body);
+    .then(response => {
+      console.log(response);
     })
     .catch(err => {
       console.log(err);
     });
+  return superagent.get("https://swapi.co/api/planets/1/");
 };
 
 module.exports = { getTatooineResidents };
