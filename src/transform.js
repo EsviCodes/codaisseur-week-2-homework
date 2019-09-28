4745272466eaa022159cbdf815b9f919c5ee6214
EsviCodes <esther.js.vis@gmail.com>
2019-09-28 17:40:50 +0200

    second time that test passes with object when array is empty

--- src/transform.js
+++ src/transform.js
@@ -1,9 +1,7 @@
 const groupAdultsByAgeRange = people => {
   //console.log(people);
   if (people.length === 0) {
-    return people.push({});
-  } else {
-    return people.filter(person => person.age < 18);
+    return {};
   }
 };
 
