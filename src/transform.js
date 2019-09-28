191b271b97787366550ad833ae44a183eff604ee
EsviCodes <esther.js.vis@gmail.com>
2019-09-28 20:41:21 +0200

    I made all the tests in the transforms section work. Whoehoe

--- src/transform.js
+++ src/transform.js
@@ -1,37 +1,56 @@
 const groupAdultsByAgeRange = people => {
   //console.log(people);
-  if (people.length === 0 || people.filter(person => person.age < 18)) {
+  if (people.length === 0) {
     return {};
-  } else {
-    const totalGroup = people.reduce(
-      (categories, currentPerson) => {
-        //console.log("Categories", categories);
-        // console.log("CurrentPerson", currentPerson);
-        if (currentPerson.age < 18) {
-          return categories;
-        } else if (currentPerson.age >= 18 && currentPerson.age <= 20) {
-          categories["20 and younger"].push(currentPerson);
-        } else if (currentPerson.age > 20 && currentPerson.age <= 30) {
-          categories["21-30"].push(currentPerson);
-        } else if (currentPerson.age > 30 && currentPerson.age <= 40) {
-          categories["31-40"].push(currentPerson);
-        } else if (currentPerson.age > 40 && currentPerson.age <= 50) {
-          categories["41-50"].push(currentPerson);
-        } else {
-          categories["51 and older"].push(currentPerson);
-        }
-        console.log(categories);
-        return categories;
-      },
-      {
-        "20 and younger": [],
-        "21-30": [],
-        "31-40": [],
-        "41-50": [],
-        "51 and older": []
-      }
-    );
   }
+
+  // LOGIC - but now I have to make it work
+  return people.reduce((categories, currentPerson) => {
+    // console.log("Categories", categories);
+    // console.log("CurrentPerson", currentPerson);
+
+    if (currentPerson.age >= 18 && currentPerson.age <= 20) {
+      //   console.log("20 and younger");
+      if (categories["20 and younger"] === undefined) {
+        categories["20 and younger"] = [currentPerson];
+      } else {
+        categories["20 and younger"].push(currentPerson);
+      }
+    }
+    if (currentPerson.age > 20 && currentPerson.age <= 30) {
+      //   console.log("21-30");
+      if (categories["21-30"] === undefined) {
+        categories["21-30"] = [currentPerson];
+      } else {
+        categories["21-30"].push(currentPerson);
+      }
+    }
+    if (currentPerson.age > 30 && currentPerson.age <= 40) {
+      //   console.log("31-40");
+      if (categories["31-40"] === undefined) {
+        categories["31-40"] = [currentPerson];
+      } else {
+        categories["31-40"].push(currentPerson);
+      }
+    }
+    if (currentPerson.age > 40 && currentPerson.age <= 50) {
+      //   console.log("41-50");
+      if (categories["41-50"] === undefined) {
+        categories["41-50"] = [currentPerson];
+      } else {
+        categories["41-50"].push(currentPerson);
+      }
+    }
+    if (currentPerson.age > 50) {
+      //   console.log("51 and older");
+      if (categories["51 and older"] === undefined) {
+        categories["51 and older"] = [currentPerson];
+      } else {
+        categories["51 and older"].push(currentPerson);
+      }
+    }
+    return categories;
+  }, {});
 };
 
 module.exports = { groupAdultsByAgeRange };
