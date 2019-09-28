62fca7140aa55a5cc90fc3f609ef9f25d1ad9d1a
EsviCodes <esther.js.vis@gmail.com>
2019-09-28 10:05:45 +0200

    Added methods to add items

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,5 +1,20 @@
 class ShoppingCart {
-  constructor() {}
+  constructor(items) {
+    this.items = [];
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(itemName, quantity, price) {
+    const item = {
+      name: itemName,
+      quantity: quantity,
+      pricePerUnit: price
+    };
+    this.items.push(item);
+  }
 }
 
 module.exports = ShoppingCart;
