bc5bc05742f116debc0f8bcf667668a9de96186f
EsviCodes <esther.js.vis@gmail.com>
2019-09-28 17:56:53 +0200

    filters out people under the age of 18

--- src/transform.js
+++ src/transform.js
@@ -1,6 +1,6 @@
 const groupAdultsByAgeRange = people => {
   //console.log(people);
-  if (people.length === 0) {
+  if (people.length === 0 || people.filter(person => person.age < 18)) {
     return {};
   }
 };
